15afb464c73b458fca948120331c1362248def0d
QuentinJouanisson <quentin.jouanisson@gmail.com>
2025-07-08 13:19:56 +0200

    init commit

--- Assets/04_Scripts/PlayerController.cs
+++ Assets/04_Scripts/PlayerController.cs
@@ -39,16 +39,42 @@ namespace Controller
         // Update is called once per frame
         void Update()
         {
+            inputVertical = Input.GetAxis("Vertical");
+            inputHorizontal = Input.GetAxis("Horizontal");
 
         }
 
-        public async void Progress()
+        void FixedUpdate()
         {
-            while (true)
+            grounded = IsGrounded();
+            
+            if (grounded)
             {
-                await Awaitable.NextFrameAsync();
+                rb.AddForce(transform.forward * inputVertical * forwardForce * Time.fixedDeltaTime);
+                rb.AddTorque(Vector3.up * inputHorizontal * TurnTorque * Time.fixedDeltaTime);
             }
+            else
+            {
+                rb.AddTorque(Vector3.up * inputHorizontal * breakingForce * Time.fixedDeltaTime);
+            }
+
+            ApplyUprightTorque();
+
         }
 
+        bool IsGrounded()
+        {
+            bool frontGrounded = Physics.Raycast(frontPoint.position, Vector3.down, groundCheckDistance, groundLayer);
+            bool rearGrounded = Physics.Raycast(rearPoint.position, Vector3.down, groundCheckDistance, groundLayer);
+            return frontGrounded || rearGrounded;
+        }
+
+        void ApplyUprightTorque()
+        {
+            Vector3 up = transform.up;
+            Vector3 desiredUp = Vector3.up;
+            Vector3 torqueVector = Vector3.Cross(up, desiredUp);
+            rb.AddTorque(torqueVector * uprightTorque - rb.angularVelocity * uprightDamping);
+        }
     }
 }
