9df8c97d0e7c2a594e28733ef5712a6f91da5b3d
QuentinJouanisson <46202843+QuentinJouanisson@users.noreply.github.com>
2025-08-28 22:27:40 +0200

    Fixed Ennemy behavior bug

--- Assets/04_Scripts/EnnemyDamageSphere.cs
+++ Assets/04_Scripts/EnnemyDamageSphere.cs
@@ -49,8 +49,7 @@ namespace Ennemy
         {
             if (other.CompareTag(playerTag))
             {
-                playerInside = false; 
-                playerHealth = null; 
+                playerInside = false;                
             }
         }
 
